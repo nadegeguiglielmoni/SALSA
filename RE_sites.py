0918738fe50092f6d03072b2bb7c562f85d1fa7a
cmdoret <cyril.mattheydoret@gmail.com>
2019-02-22 13:51:29 +0100

    reformat code to black, remove obsolete variables and convert to python3

--- RE_sites.py
+++ RE_sites.py
@@ -3,7 +3,7 @@ import argparse
 import seq_utils as sequ
 
 #======================================================================#
-#								ARGUMENTS
+#                                ARGUMENTS
 #======================================================================#
 
 parser = argparse.ArgumentParser()
@@ -17,55 +17,55 @@ parser.add_argument("-e", "--enzyme", help="restriction enzyme")
 args = parser.parse_args()
 
 #======================================================================#
-#								MODULES
+#                                MODULES
 #======================================================================#
 
 # PARSE_ENZYME_INPUT
 # input :
-#	enzyme_input : str, one or several enzymes separated by commas
+#    enzyme_input : str, one or several enzymes separated by commas
 # output :
-#	enzyme_list : list, list of enzymes
+#    enzyme_list : list, list of enzymes
 def parse_enzyme_input(enzyme_input: str) -> list :
-	"""
-	Parse the list of enzymes given as input.
-	"""
-	enzyme_list = args.enzyme.strip(" ").split(',')
-	return enzyme_list
-	
+    """
+    Parse the list of enzymes given as input.
+    """
+    enzyme_list = args.enzyme.strip(" ").split(',')
+    return enzyme_list
+    
 def main():
-	seq_data = sequ.parse_fasta(args.assembly)
-	
-	# Parse one or several enzymes given as input
-	enzymes_input = parse_enzyme_input(args.enzyme)
+    seq_data = sequ.parse_fasta(args.assembly)
+    
+    # Parse one or several enzymes given as input
+    enzymes_input = parse_enzyme_input(args.enzyme)
 
-	final_enzymes = []
-	for each in enzymes_input:
-		if 'N' in each:
-			final_enzymes.append(each.replace('N','G'))
-			final_enzymes.append(each.replace('N','A'))
-			final_enzymes.append(each.replace('N','T'))
-			final_enzymes.append(each.replace('N','C'))
-		else:
-			final_enzymes.append(each)
+    final_enzymes = []
+    for each in enzymes_input:
+        if 'N' in each:
+            final_enzymes.append(each.replace('N','G'))
+            final_enzymes.append(each.replace('N','A'))
+            final_enzymes.append(each.replace('N','T'))
+            final_enzymes.append(each.replace('N','C'))
+        else:
+            final_enzymes.append(each)
 
-	for key in seq_data :
-		
-		id_seq, seq = key, f[key]
-		left_count = 0
-		rigt_count = 0
-		for enzyme in final_enzymes :
-			pos  = [m.start(0) for m in re.finditer(enzyme,seq)]
-		 
-			length = len(seq)	
-			for each in pos:
-				if each < length/2:
-					left_count += 1
-				else:
-					rigt_count += 1
+    for key in seq_data :
+        
+        id_seq, seq = key, f[key]
+        left_count = 0
+        rigt_count = 0
+        for enzyme in final_enzymes :
+            pos  = [m.start(0) for m in re.finditer(enzyme,seq)]
+         
+            length = len(seq)    
+            for each in pos:
+                if each < length/2:
+                    left_count += 1
+                else:
+                    rigt_count += 1
 
-		print("{0}\t{1}\t{2}\n".format(id, left_count, rigt_count))
+        print("{0}\t{1}\t{2}\n".format(id, left_count, rigt_count))
 
-	
+    
 
 if __name__ == '__main__':
-	main()
+    main()
--- get_seq.py
+++ get_seq.py
@@ -2,7 +2,7 @@ import pickle
 import argparse
 import seq_utils as sequ
 
-#======================= ARGUMENTS ====================================#
+# ======================= ARGUMENTS ====================================#
 
 parser = argparse.ArgumentParser()
 
@@ -13,8 +13,8 @@ parser.add_argument("-p", "--map", help="pickle map of scaffolds")
 
 args = parser.parse_args()
 
-#======================================================================#						
-scaff_map = pickle.load(open(args.map,'r'))
+# ======================================================================#
+scaff_map = pickle.load(open(args.map, "r"))
 
 contig_length = {}
 id2seq = {}
@@ -22,76 +22,76 @@ id2seq = {}
 id2seq = sequ.parse_fasta(args.cleaned)
 
 for key in id2seq:
-	id2seq[key] = id2seq[key]
-	contig_length[key] = len(id2seq[key])
+    id2seq[key] = id2seq[key]
+    contig_length[key] = len(id2seq[key])
 
-#first sort scaffolds in decreasing order of length
+# first sort scaffolds in decreasing order of length
 
 scaff2length = {}
 for scaffold in scaff_map:
-	path = scaff_map[scaffold]
-	length = 0
-	for i in xrange(0,len(path)-1,2):
-		length += contig_length[path[i].split(':')[0]]
-	scaff2length[scaffold] = length
+    path = scaff_map[scaffold]
+    length = 0
+    for i in xrange(0, len(path) - 1, 2):
+        length += contig_length[path[i].split(":")[0]]
+    scaff2length[scaffold] = length
 
-sorted_scaffolds = sorted(scaff2length.items(), key=lambda x: x[1],reverse=True)
+sorted_scaffolds = sorted(scaff2length.items(), key=lambda x: x[1], reverse=True)
 
 c_id = 1
 line = ""
-agp_output = open(args.agp, 'w')
-ofile = open(args.scaffold, 'w')
+agp_output = open(args.agp, "w")
+ofile = open(args.scaffold, "w")
 
 for key in sorted_scaffolds:
-	#print 'scaffold_'+str(c_id) + '\t' + key
-	key = key[0]
-	start = 1
-	local_comp = 1
-	#if len(scaff_map[key]) >= 4:
-	path = scaff_map[key]
-	scaff_len = 0
-	curr_contig = ""
-	#print c_id
-	line = ''
-	for i in range(0,len(path)-1,2):
-		line += "scaffold_" + str(c_id) + '\t' + str(start) + str('\t')
-		curr = path[i]
-		nextitem = path[i+1]
-		curr = curr.split(':')
-		nextitem = nextitem.split(':')
-		curr_len = contig_length[curr[0]]
-		scaff_len += curr_len
-		end = curr_len + start - 1
-		line += str(end) + '\t'
-		start = end + 1
-		line += str(local_comp)
-		local_comp += 1
-		line += ('\tW\t' + curr[0] + '\t' + '1\t')
-		line += str(curr_len) + '\t'
-		#print curr
-		if curr[1] == 'B' and nextitem[1] == 'E':
-			curr_contig += id2seq[curr[0]]
-			line += '+\t'
-		if curr[1] == 'E' and nextitem[1] == 'B':
-			line += '-\t'
-			curr_contig += sequ.rev_comp(id2seq[curr[0]])
+    # print 'scaffold_'+str(c_id) + '\t' + key
+    key = key[0]
+    start = 1
+    local_comp = 1
+    # if len(scaff_map[key]) >= 4:
+    path = scaff_map[key]
+    scaff_len = 0
+    curr_contig = ""
+    # print c_id
+    line = ""
+    for i in range(0, len(path) - 1, 2):
+        line += "scaffold_" + str(c_id) + "\t" + str(start) + str("\t")
+        curr = path[i]
+        nextitem = path[i + 1]
+        curr = curr.split(":")
+        nextitem = nextitem.split(":")
+        curr_len = contig_length[curr[0]]
+        scaff_len += curr_len
+        end = curr_len + start - 1
+        line += str(end) + "\t"
+        start = end + 1
+        line += str(local_comp)
+        local_comp += 1
+        line += "\tW\t" + curr[0] + "\t" + "1\t"
+        line += str(curr_len) + "\t"
+        # print curr
+        if curr[1] == "B" and nextitem[1] == "E":
+            curr_contig += id2seq[curr[0]]
+            line += "+\t"
+        if curr[1] == "E" and nextitem[1] == "B":
+            line += "-\t"
+            curr_contig += sequ.rev_comp(id2seq[curr[0]])
 
-		agp_output.write(line+'\n')
-		if i != len(path) - 2:
-			for j in range(0,500):
-				curr_contig += 'N'
-			line = 'scaffold_' + str(c_id) + '\t' + str(start) + '\t'
-			end = 500 + start - 1
-			line += str(end) + '\t'
-			start = end + 1
-			line += str(local_comp) + '\t'
-			local_comp += 1
-			line += 'N\t500\tscaffold\tyes\tna'
-			agp_output.write(line + '\n')
-			line = ""
-			
-	chunks = [curr_contig[i:i+80] for i in xrange(0,len(curr_contig),80)]
-	ofile.write('>scaffold_'+str(c_id)+'\n')
-	for chunk in chunks:
-		ofile.write(chunk+'\n')
-	c_id += 1
+        agp_output.write(line + "\n")
+        if i != len(path) - 2:
+            for j in range(0, 500):
+                curr_contig += "N"
+            line = "scaffold_" + str(c_id) + "\t" + str(start) + "\t"
+            end = 500 + start - 1
+            line += str(end) + "\t"
+            start = end + 1
+            line += str(local_comp) + "\t"
+            local_comp += 1
+            line += "N\t500\tscaffold\tyes\tna"
+            agp_output.write(line + "\n")
+            line = ""
+
+    chunks = [curr_contig[i : i + 80] for i in xrange(0, len(curr_contig), 80)]
+    ofile.write(">scaffold_" + str(c_id) + "\n")
+    for chunk in chunks:
+        ofile.write(chunk + "\n")
+    c_id += 1
